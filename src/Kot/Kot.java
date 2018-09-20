52610f1b829b02b2f167e4ffc5a95593791cbd52
Wojciech T <hadwao2@gmail.com>
2018-09-20 20:02:51 +0200

    4.4 dodatkowe

---
+++ src/Kot/Kot.java
@@ -0,0 +1,11 @@
+package Kot;
+
+public class Kot {
+
+
+    public static void main(String[] args) {
+        System.out.print((char)0x4B);
+        System.out.print((char)0x4F);
+        System.out.print((char)0x54);
+    }
+}
