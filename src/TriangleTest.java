dcfe8d180239aefba963f0d7adb94085989cf9c5
WojciechTom <42958849+WojciechTom@users.noreply.github.com>
2018-09-20 20:05:03 +0200

    Delete TriangleTest.java

--- src/TriangleTest.java
+++
@@ -1,9 +0,0 @@
-import java.util.concurrent.TransferQueue;
-
-public class TriangleTest {
-    public static void main(String[] args) {
-        Triangle trojkat1 = new Triangle(4,4,5);
-        TriangleChecker maszyna = new TriangleChecker();
-        System.out.println(maszyna.isRightTriangle(trojkat1));
-    }
-}
